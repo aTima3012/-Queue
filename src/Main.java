e03c2a16416f6c3513e2f154d9683ce09dac549a
aTima3012 <3012@inbox.ru>
2023-01-31 22:47:41 +0400

    reformat

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,19 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
         Person person;
-
         Queue<Person> list = new LinkedList<>(generateClients());
 
-                while (!list.isEmpty()) {
-                    person = list.poll();
-
-                    if (person.ticket != 0) {
-                        System.out.println(person + " прокатился на атракционе ");
-                        person.ticket -=1;
-                        list.offer(person);
-
-                    }
-                }
+        while (!list.isEmpty()) {
+            person = list.poll();
+            if (person.ticket != 0) {
+                System.out.println(person + " прокатился на атракционе ");
+                person.ticket -= 1;
+                list.offer(person);
+            }
         }
-        public static List<Person>generateClients() {
+    }
+
+    public static List<Person> generateClients() {
         List<Person> list = new LinkedList<>();
         list.add(new Person("Наташа", "Иванова", 6));
         list.add(new Person("Иммануил", "Кант", 8));
